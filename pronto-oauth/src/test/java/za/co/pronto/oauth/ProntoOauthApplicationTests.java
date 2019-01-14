888831b4dd01340bbb836ce912121ae0536e6f2a
thabiso.shangase <shangasethabiso@gmail.com>
2019-01-14 19:49:50 +0200

    yonke into engnayo kuze kube ila

---
+++ pronto-oauth/src/test/java/za/co/pronto/oauth/ProntoOauthApplicationTests.java
@@ -0,0 +1,16 @@
+package za.co.pronto.oauth;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProntoOauthApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
